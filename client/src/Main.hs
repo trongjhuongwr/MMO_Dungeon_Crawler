b160671a6de4d9a24685501114e2ace8ba01d699
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2025-10-12 23:12:40 +0700

    fix angle

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -52,11 +52,11 @@ loadResources :: IO (Either String (Picture, Picture))
 loadResources = do
   mTankBody <- loadJuicyPNG "client/assets/textures/tanks/rapid_tank/body.png"
   case mTankBody of
-    Nothing -> return $ Left "Failed to load tank body: client/assets/textures/tanks/rapid_tank/body.png"
+    Nothing -> return $ Left "Failed to load tank body"
     Just tankBody -> do
       mTankTurret <- loadJuicyPNG "client/assets/textures/tanks/rapid_tank/turret.png"
       case mTankTurret of
-        Nothing -> return $ Left "Failed to load tank turret: client/assets/textures/tanks/rapid_tank/turret.png"
+        Nothing -> return $ Left "Failed to load tank turret"
         Just tankTurret -> return $ Right (tankBody, tankTurret)
 
 -- | Hàm chạy game chính sau khi đã có đủ mọi thứ
@@ -76,8 +76,8 @@ runGame serverAddr sock (tankBody, tankTurret) = do
     60
     initialClientState
     (\_ -> render (tankBody, tankTurret) . csWorld <$> readMVar clientStateRef)
-    (handleInput serverAddr sock)
-    updateClient
+    handleInput
+    (updateClient serverAddr sock)
 
 -- | Vòng lặp nhận dữ liệu từ server
 networkListenLoop :: Socket -> MVar ClientState -> IO ()
@@ -94,26 +94,23 @@ networkListenLoop sock stateRef = do
       networkListenLoop sock stateRef
 
 -- | Xử lý input từ Gloss
-handleInput :: SockAddr -> Socket -> Event -> ClientState -> IO ClientState
-handleInput serverAddr sock (EventKey key Down _ _) cs = do
+handleInput :: Event -> ClientState -> IO ClientState
+handleInput (EventKey key Down _ _) cs = do
   let newKeys = Set.insert key (csKeys cs)
-  sendCmdToServer serverAddr sock newKeys (csMousePos cs)
   return cs { csKeys = newKeys }
-handleInput serverAddr sock (EventKey key Up _ _) cs = do
+handleInput (EventKey key Up _ _) cs = do
   let newKeys = Set.delete key (csKeys cs)
-  sendCmdToServer serverAddr sock newKeys (csMousePos cs)
   return cs { csKeys = newKeys }
-handleInput serverAddr sock (EventMotion pos) cs = do
-  sendCmdToServer serverAddr sock (csKeys cs) pos
+handleInput (EventMotion pos) cs = do
   return cs { csMousePos = pos }
-handleInput _ _ _ cs = return cs
+handleInput _ cs = return cs
 
 -- | Gửi lệnh tới server
 sendCmdToServer :: SockAddr -> Socket -> KeyMap -> (Float, Float) -> IO ()
 sendCmdToServer serverAddr sock keys (mouseX, mouseY) = do
-  let moveVec = calculateMoveVector keys
-  let turretAngle = atan2 mouseY mouseX
-  let command = MoveAndAim moveVec turretAngle
+  let moveVec     = calculateMoveVector keys
+  let turretAngle = atan2 mouseY mouseX - (pi / 2) -- Trừ đi 90 độ (pi/2) để bù lại hướng của asset
+  let command     = MoveAndAim moveVec (-turretAngle) -- Vẫn giữ đảo ngược góc quay
   putStrLn $ "[DEBUG] Sending command: " ++ show command
   let lazyMsg = encode command
   let strictMsg = toStrict lazyMsg
@@ -121,5 +118,7 @@ sendCmdToServer serverAddr sock keys (mouseX, mouseY) = do
   return ()
 
 -- | Update state của client mỗi frame
-updateClient :: Float -> ClientState -> IO ClientState
-updateClient _dt cs = return cs
+updateClient :: SockAddr -> Socket -> Float -> ClientState -> IO ClientState
+updateClient serverAddr sock _dt cs = do
+  sendCmdToServer serverAddr sock (csKeys cs) (csMousePos cs)
+  return cs
