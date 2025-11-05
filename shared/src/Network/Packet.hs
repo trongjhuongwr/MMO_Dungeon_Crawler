997b50912334024b31c8b8f97249504477f34903
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2025-11-05 14:22:23 +0700

    define type

--- shared/src/Network/Packet.hs
+++ shared/src/Network/Packet.hs
@@ -7,6 +7,7 @@ import GHC.Generics (Generic)
 import Types.Player (PlayerState)
 import Types.Bullet (BulletState)
 import Types.Enemy (EnemyState)
+import Types.Map (GameMap)
 
 -- | Trạng thái của toàn bộ thế giới game tại một thời điểm.
 -- | THAY ĐỔI: Chuyển từ 'newtype' sang 'data' vì có nhiều hơn 1 trường.
@@ -14,6 +15,7 @@ data WorldSnapshot = WorldSnapshot
   { wsPlayers :: [PlayerState]
   , wsEnemies :: [EnemyState]
   , wsBullets :: [BulletState]
+  , wsMap     :: GameMap
   } deriving (Show, Generic)
 
 instance Binary WorldSnapshot
